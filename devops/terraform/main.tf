df3af708499e734bb3437425c4272ebe594a8fcc
vamshi21401 <130837163+vamshi21401@users.noreply.github.com>
2024-02-08 18:05:12 +0530

    Update main.tf

--- devops/terraform/main.tf
+++ devops/terraform/main.tf
@@ -10,7 +10,7 @@ security_groups = [aws_security_group.web_server.id]
 subnets = ["subnet-03c56132e625b3ada", "subnet-0e0d23dac85a42572"]
 listener {
 instance_port = 8000
-instance_portocol = "http"
+instance_protocol = "http"
 lb_port = 80
 lb_protocol = "http"
 }
@@ -27,7 +27,7 @@ max_size = 3
 desired_capacity = 2
 health_check_type = "EC2"
 load_balancers = [aws_elb.web_server_lb.name]
-availbility_zones ["us-east-1d", "us-east-1c"]
+availability_zones = ["us-east-1d", "us-east-1c"]
 }
 
 
