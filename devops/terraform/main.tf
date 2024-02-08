b278440ff248df68fa63bfc8864c71018d08cf4f
vamshi21401 <130837163+vamshi21401@users.noreply.github.com>
2024-02-08 19:07:51 +0530

    Update main.tf

--- devops/terraform/main.tf
+++ devops/terraform/main.tf
@@ -7,7 +7,7 @@ key_name = "ansible"
 resource "aws_elb" "web_server_lb" {
 name = "web-server-lb"
 security_groups = [aws_security_group.web_server.id]
-subnets = ["subnet-014ada08800354022", "subnet-03c56132e625b3ada"]
+subnets = ["subnet-0203f1de684ad9515", "subnet-0e31a79aca92aee3f"]
 listener {
 instance_port = 8000
 instance_protocol = "http"
@@ -26,7 +26,7 @@ resource "aws_autoscaling_group" "web_server_asg" {
     desired_capacity     = 2
     health_check_type    = "EC2"
     load_balancers       = [aws_elb.web_server_lb.name]
-    availability_zones    = ["us-east-1b", "us-east-1d"] 
+    availability_zones    = ["us-east-1a", "us-east-1b"] 
     
   }
 
